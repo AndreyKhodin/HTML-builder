c21f5f996b4496dc128488521b408e6d7d56aa21
Andrei Khodin <khodin9@gmail.com>
2022-06-08 20:40:01 +0300

    ReadStream change

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,14 @@
 let fs = require('fs');
-let fileContent = fs.readFileSync('text.txt', 'utf8');
-console.log(fileContent);
+//let fileContent = fs.readFileSync('text.txt', 'utf8');
+//console.log(fileContent);
+
+var stream = new fs.ReadStream('text.txt', {encoding: 'utf-8'});
+ 
+stream.on('readable', function(){
+    var data = stream.read();
+    console.log(data);
+});
+ 
+ //stream.on('end', function(){
+ //   console.log("THE END");
+//});
